99bed4fd4089c42864a6724c24badd14ca8d44ec
Ben <ncor89b@gmail.com>
2022-07-30 18:05:20 +0200

    API data collected and displayed start of app design

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,40 +1,35 @@
-/*
-const postSection = document.querySelector('#posts__boxes');
-const postTemplate = document.querySelector('#template__boxes');
+const postSection = document.querySelector("#posts__boxes");
+const postTemplate = document.querySelector("#template__boxes");
 
-
-( async () => {
-const url = 'https://jsonplaceholder.typicode.com/posts'
-const fetchData = await fetch(url);
+(async () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const fetchData = await fetch(url);
   const jsonData = await fetchData.json();
   let i = 0;
-  jsonData.forEach(getValue => {
-    i++
-    if(i < 11) {
-    
-    const title = getValue.title;
-    const body = getValue.body;
-    
-    fetch('https://unsplash.it/300/200')
-    .then(res => res.blob())
-    .then(blob => {
-      
-    const newPost = document.importNode(postTemplate.content, true);
-    
-  let displayName = newPost.querySelector("#box__name");
-  let displayBody = newPost.getElementById("box__body");
-  let displayImg =newPost.querySelector('.box__img');
-      
-    displayImg.src = URL.createObjectURL(blob)
-    displayName.innerText = title;
-    displayBody.innerText = body;
-    postSection.appendChild(newPost);
-    })
-    .catch(err => {
-      console.error(err)
-    })
-   } 
- })
-  
+  jsonData.forEach((getValue) => {
+    i++;
+    if (i < 11) {
+      const title = getValue.title;
+      const body = getValue.body;
+      console.log(title);
+      fetch("https://unsplash.it/300/200")
+        .then((res) => res.blob())
+        .then((blob) => {
+          const newPost = document.importNode(postTemplate.content, true);
+
+          let displayName = newPost.querySelector("#box__title");
+          console.log(displayName);
+          let displayBody = newPost.getElementById("box__content");
+          let displayImg = newPost.querySelector(".box__img");
+
+          displayImg.src = URL.createObjectURL(blob);
+          displayName.innerText = title;
+          displayBody.innerText = body;
+          postSection.appendChild(newPost);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  });
 })();
-*/
